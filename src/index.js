a7c00162ac111f294ce3acd77e7a1b720287e580
sashkill94 <sashkill94@gmail.com>
2022-07-31 19:46:55 +0300

    init: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let string = String(Math.abs(n));
+    string = [...string].reverse().join("");
+    console.log(string);
+    string = Number(string)
+    console.log(string);
+    
+    return string;
 }
